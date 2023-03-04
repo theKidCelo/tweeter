819bcdaebd42620f1ac18f0789c72c82e362b0e5
Ramy Mohamed <ramy.opene@gmail.com>
2023-03-04 15:59:39 -0500

    made bug fixes with counter

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,49 +4,53 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function() {
-    // Function to prevent XSS. createTextNode escapes HTML characters 'e.g. <>'
-    const escape = function (str) {
-      let div = document.createElement("div");
-      div.appendChild(document.createTextNode(str));
-      return div.innerHTML;
-    };
-  
-    // Function to asynchronously fetch tweet data from '/tweets' via AJAX GET
-    // If successful, it will call the renderTweets function above and pass it the JSON response data that is returned from the request as an argument
-    // This function will also be invoked after every successful POST request when a tweet is submitted
-    const loadTweets = function() {
-      $.ajax({
-        url: '/tweets',
-        type: 'GET',
-      })
+$(document).ready(function () {
+  // Function to prevent XSS. createTextNode escapes HTML characters 'e.g. <>'
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  // Function to asynchronously fetch tweet data from '/tweets' via AJAX GET
+  // If successful, it will call the renderTweets function above and pass it the JSON response data that is returned from the request as an argument
+  // This function will also be invoked after every successful POST request when a tweet is submitted
+  const loadTweets = function () {
+    $.ajax({
+      url: "/tweets",
+      type: "GET",
+    })
       .then((response) => {
         renderTweets(response);
       })
       .catch((error) => {
-        console.log('error:', error)
-      })
+        console.log("error:", error);
+      });
+  };
+
+  loadTweets();
+
+  // Checks whether the avatars property contains an http link or an svg class from one of the project libraries
+  // For instance, this can be a valid avatar property without an http link --> "avatars": "<i class=\"fas fa-user-secret fa-2x\"></i>"
+  const avatarType = function (avatar) {
+    const regex = new RegExp("^http");
+
+    if (regex.test(avatar)) {
+      return `<img src=${avatar}/>`;
+    } else {
+      return avatar;
     }
-  
-    loadTweets();
-  
-    // Checks whether the avatars property contains an http link or an svg class from one of the project libraries
-    // For instance, this can be a valid avatar property without an http link --> "avatars": "<i class=\"fas fa-user-secret fa-2x\"></i>"
-    const avatarType = function(avatar) {
-      const regex = new RegExp("^http");
-  
-      if (regex.test(avatar)) {
-        return `<img src=${avatar}/>`;
-      } else {
-        return avatar;
-      }
-    };
-  
-    const createTweetElement = function(obj) {
-      const $tweet = `
+  };
+
+  const createTweetElement = function (obj) {
+    const $tweet = `
         <article class = "tweet-container">
             <header>
-              ${obj.user.avatars ? avatarType(obj.user.avatars) : '<i class="fas fa-user-secret fa-2x"></i>'}
+              ${
+                obj.user.avatars
+                  ? avatarType(obj.user.avatars)
+                  : '<i class="fas fa-user-secret fa-2x"></i>'
+              }
               <div class="username">
                 ${obj.user.name}
               </div>
@@ -69,57 +73,54 @@ $(document).ready(function() {
             </footer>
           </article>
       `;
-      return $tweet;
-    };
-  
-    const renderTweets = function(tweets) {
-      // loops through tweets
-      $('#tweets-container').empty();
-      for (let tweetObj of tweets) {
-        // calls createTweetElement for each tweet
-        // takes return value and appends it to the tweets container
-        const $tweet = createTweetElement(tweetObj);
-        $('#tweets-container').prepend($tweet);
-      }
-    };
-  
-    // prevent page from navigating upon form submission
-    $('.new-tweet form').on('submit', function(e) {
-      // prevent default form behaviour
-      e.preventDefault();
-      const $inputField = $(this).find('#tweet-text').val();
-      // remove any present error messages upon submit
-      $('#error-blank').slideUp();
-      $('#error-exceed').slideUp();
-  
-      // validation for tweet lengths
-      if ($inputField.length < 1) {
-        $('#error-blank').slideDown();
-      } else if ($inputField.length > 140) {
-        $('#error-exceed').slideDown();
-      } else {
-        const $formData = $(this).serialize();
-        
-        // asynchronously send the data to server
-        $.ajax({
-          url: '/tweets',
-          type: 'POST',
-          data: $formData
-        })
+    return $tweet;
+  };
+
+  const renderTweets = function (tweets) {
+    // loops through tweets
+    $("#tweets-container").empty();
+    for (let tweetObj of tweets) {
+      // calls createTweetElement for each tweet
+      // takes return value and appends it to the tweets container
+      const $tweet = createTweetElement(tweetObj);
+      $("#tweets-container").prepend($tweet);
+    }
+  };
+
+  // prevent page from navigating upon form submission
+  $(".new-tweet form").on("submit", function (e) {
+    // prevent default form behaviour
+    e.preventDefault();
+    const $inputField = $(this).find("#tweet-text").val();
+    // remove any present error messages upon submit
+    $("#error-blank").slideUp();
+    $("#error-exceed").slideUp();
+
+    // validation for tweet lengths
+    if ($inputField.length < 1) {
+      $("#error-blank").slideDown();
+    } else if ($inputField.length > 140) {
+      $("#error-exceed").slideDown();
+    } else {
+      const $formData = $(this).serialize();
+
+      // asynchronously send the data to server
+      $.ajax({
+        url: "/tweets",
+        type: "POST",
+        data: $formData,
+      })
         .then(() => {
           // clear new tweet input field
-          $(this).children('#tweet-text').val('');
-          // reset counter 
-          $(this).find('.counter').html('140');
+          $(this).children("#tweet-text").val("");
+          // reset counter
+          $(this).find(".counter").html("140");
           // fetch tweets from DB and render them to page
           loadTweets();
         })
         .catch((error) => {
-          console.log('error:', error)
-        })
-      }
-    });
-    
-  
+          console.log("error:", error);
+        });
+    }
   });
-  
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,15 @@
-$(document).ready(function() {
-
-  $('#tweet-text').on('input', function() {
+$(document).ready(function () {
+  $("#tweet-text").on("input", function () {
     // assign textarea's sibling child element (.counter) a variable
-    const $counter = $(this).next().children('.counter');
+    const $counter = $(this).next().children(".counter");
     const remainingChars = 140 - this.value.length;
 
-    // change inner html of $counter upon char input
     $counter.html(remainingChars);
 
-    // change counter colour depending on chars remaining
     if (remainingChars < 0) {
-      $counter.addClass('red');
+      $counter.addClass("red");
     } else {
-      $counter.removeClass('red');
+      $counter.removeClass("red");
     }
   });
-
 });
